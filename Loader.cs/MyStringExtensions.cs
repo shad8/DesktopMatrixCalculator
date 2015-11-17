7f5db7305100d7a1927fa267fbcac57c47408381
shad8 <uholodniak@gmail.com>
2015-11-17 22:51:12 +0100

    Matrix format support dot and comma separator

--- Loader.cs/MyStringExtensions.cs
+++ Loader.cs/MyStringExtensions.cs
@@ -6,10 +6,16 @@ namespace MatrixFileManager
 {
   public static class MyStringExtensions
   {
-    public static double[] ConvertStringToDoubleArray(this string value, string sign)
+    public static double[] ConvertStringToDoubleArray(this string value, string separator)
     {
-      //TODO set support . and , 
-      string[] strings = value.Split(new string[] { sign }, StringSplitOptions.RemoveEmptyEntries);
+      if (value.Contains(".") || value.Contains(",")) {
+        if (CultureInfo.CurrentCulture.Name == "pl-PL" || CultureInfo.CurrentCulture.Name == "pl")
+          value = value.Replace(".", ",");
+        else
+          value = value.Replace(",", ".");
+      }
+      
+      string[] strings = value.Split(new string[] { separator }, StringSplitOptions.RemoveEmptyEntries);
       double[] data = strings.Skip(1).Select(x => double.Parse(x, CultureInfo.CurrentCulture)).ToArray();
       return data;
     }
